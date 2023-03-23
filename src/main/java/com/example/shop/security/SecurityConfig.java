c6e2422a2d7d3b14b8b29380f76e5ac91cf5e087
Dawid <dawid.rrozewski@gmail.com>
2023-03-23 14:29:24 +0100

    logowanie użytkownika administracyjnego (JWT token)

---
+++ src/main/java/com/example/shop/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.shop.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.activation.DataSource;
+
+@Configuration
+public class SecurityConfig {
+
+    private String secret;
+
+    public SecurityConfig(@Value("${jwt.secret}") String secret) {
+        this.secret = secret;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           AuthenticationManager authenticationManager,
+                                           UserDetailsService userDetailsService) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .antMatchers("/admin/**").authenticated()
+                .anyRequest().permitAll());
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilter(new JwtAuthorizationFilter(authenticationManager, userDetailsService, secret));
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+}
